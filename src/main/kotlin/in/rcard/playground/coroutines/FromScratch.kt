ce8c0fe689298999958591d8c1dfe1a823b42659
rcardin <riccardo.cardin@gmail.com>
2023-07-11 17:22:44 +0200

    Added factorial and sum

--- src/main/kotlin/in/rcard/playground/coroutines/FromScratch.kt
+++ src/main/kotlin/in/rcard/playground/coroutines/FromScratch.kt
@@ -2,6 +2,7 @@ package `in`.rcard.playground.coroutines
 
 import kotlin.system.exitProcess
 
+
 interface Cont<R> {
     fun apply(result: R)
 }
@@ -15,6 +16,10 @@ fun add(a: Int, b: Int, cont: Cont<Int>) {
     cont.apply(a + b)
 }
 
+fun multiply(a: Int, b: Int, cont: Cont<Int>) {
+    cont.apply(a * b)
+}
+
 fun print(n: Int, cont: Cont<Unit>) {
     println(n)
     cont.apply(Unit)
@@ -24,18 +29,46 @@ fun print(n: Int, cont: Cont<Unit>) {
 // System.out.println(1 + 2 + 3);
 // System.exit(0);
 
+// static long sum(int from, int to) {
+//   long sum = 0;
+//   for (int i = from; i <= to; i++) {
+//     sum += i;
+//   }
+//   return sum;
+// }
+
+// }
+
+// static long sum_rec(int from, int to) {
+//   if (from > to) {
+//     return 0;
+//   } else {
+//     return from + sum_rec(from + 1, to);
+//   }
+fun sum_rec(from: Int, to: Int, cont: Cont<Int>): Unit =
+    if (from > to) {
+        cont.apply(0)
+    } else {
+        add(from, 1, Cont { from1 -> sum_rec(from1, to, Cont { r2 -> add(from, r2, cont) }) })
+    }
+
+// static int factorial(int n) {
+//   if (n == 0) return 1;
+//   return factorial(n-1)*n;
+// }
+fun factorial(n: Int, cont: Cont<Int>): Unit =
+    if (n == 0) cont.apply(1)
+    else {
+        add(n, -1, Cont { n1 -> factorial(n1, Cont { r -> multiply(n, r, cont) }) })
+    }
+
+// fun fib1(n: Int): Int {
+//    return if (n < 2) 1 else fib1(n - 1) + fib1(n - 2)
+// }
+// TODO
+
+val exit = Cont<Unit> { exitProcess(0) }
+
 fun main() {
-    add(
-        1,
-        2,
-        Cont { r1 ->
-            add(
-                r1,
-                3,
-                Cont { r2 ->
-                    print(r2, Cont { exitProcess(0) })
-                },
-            )
-        },
-    )
+    factorial(6, Cont { result -> print(result, exit) })
 }
